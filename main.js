bae4aae134d87fe874f00673580037735338f1ec
Calo Corp <marc.calo@codeimmersives.com>
2020-06-25 11:00:06 -0400

    c2

--- main.js
+++ main.js
@@ -23,19 +23,19 @@ const onlyVowels = function(str){
 challenge(2)
 
 const firstFiveVowels = function(str){
-let vowels = ['a','i','e','o','u']
+let count = 0
+    let vowels ='aeiou'
 
-for (let i=0; i <= str.length;i++){
+for (let i=0; i < str.length;i++){
     
-    if (str[i].includes(vowels)){
+    if (vowels.includes(str[i]) && count < 5){
     console.log(str[i])
+    count++
 }
-    
     }   
-   
-}
+   }
 
-firstFiveVowels("hello")
+firstFiveVowels("Regular expressions are for term 2.")
 
 challenge(3)
 
@@ -47,4 +47,4 @@ const every3rd = function (str){
 }
 }
 
-every3rd('I am the alfalfa and the omelette.')
+
