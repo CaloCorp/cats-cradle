4caec85044a2b969c661a8189c2c2680bb8c9e9a
Calo Corp <marc.calo@codeimmersives.com>
2020-06-25 11:54:43 -0400

    c5

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ challenge(2)
 
 const firstFiveVowels = function(str){
 let count = 0
-    let vowels ='aeiou'
+    let vowels =['a','e','i','o','u']
 
 for (let i=0; i < str.length;i++){
     
@@ -41,10 +41,45 @@ challenge(3)
 
 const every3rd = function (str){
     
-    for (let i=0; i <= str.length;i+3){
+    for (let i=2; i < str.length;i+=3){
 
-        console.log(str[i])
+        console.log(str[i]);
 }
 }
 
 
+every3rd('I am the alfalfa and the omelette.')
+challenge(4)
+const first4char = function(str, start){
+
+    let count =0
+    let i= start
+    while (i < str.length){
+
+        if (count < 4 ){
+            console.log(str[i])
+            count++
+        }
+        i++
+    }
+
+}
+
+first4char(`Oh hi, I didn't see you there. Welcome.`, 4)
+
+first4char(`Oh hi, I didn't see you there. Welcome.`, 36)
+
+const indexOfU= function(str){
+let i= 0
+while (i < str.length){
+if (str[i]=== 'u'){
+    console.log(i)
+}
+    i++
+}
+
+}
+
+
+challenge(5)
+indexOfU('You picked the wrong house, bub.')
